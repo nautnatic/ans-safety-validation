d8de1f5125690192fba4cddabc6d0ae8ed27ff7f
Michael Lauter <michael.lauter1@gmail.com>
2023-01-15 17:17:12 +0100

    use host user also inside container to avoid file permission problems in mounted volumes

--- runtime/build.sh
+++ runtime/build.sh
@@ -1,1 +1,7 @@
-docker build -t arena ${PROJECT_DIR}/runtime
+# define the id of the current host user as the user run the
+# commands and own the files inside the container. This is necessary
+# to avoid file permission problems in the mounted volumes.
+docker build \
+  --build-arg USER_ID="$(id -u "${USER}")" \
+  --build-arg GROUP_ID="$(id -g "${USER}")" \
+  -t arena "${PROJECT_DIR}"/runtime
